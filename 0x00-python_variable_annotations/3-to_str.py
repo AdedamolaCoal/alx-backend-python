14a6068f3915f3e84f388f9995c80946af57f52b
AdedamolaCoal <117821207+AdedamolaCoal@users.noreply.github.com>
2024-10-11 10:33:20 +0100

    task 3 done

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+3-to_str
+This module returns a string representation of an object
+"""
+
+
+def to_str(n: float) -> str:
+    """_summary_
+
+    Args:
+      n (float): takes an argument that is a float
+
+    Returns:
+      str: the return statement should be a string
+    """
+    return str(n)
