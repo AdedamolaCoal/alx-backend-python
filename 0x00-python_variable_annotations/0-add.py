295038b7b80060e67f342468302f381576b2c664
AdedamolaCoal <117821207+AdedamolaCoal@users.noreply.github.com>
2024-10-11 07:30:07 +0100

    task 0 done

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+0-add
+This module add two typed numbers together
+"""
+
+
+def add(a: float, b: float) -> float:
+    """_summary_
+
+    Args:
+        a (float): _description_
+        b (float): _description_
+
+    Returns:
+        float: _description_
+    """
+    return a + b
