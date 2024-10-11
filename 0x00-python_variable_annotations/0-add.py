1632e55e32cd3ab2990849ce6dda19b05c147c91
AdedamolaCoal <117821207+AdedamolaCoal@users.noreply.github.com>
2024-10-11 07:39:32 +0100

    update

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -9,10 +9,10 @@ def add(a: float, b: float) -> float:
     """_summary_
 
     Args:
-        a (float): _description_
-        b (float): _description_
+        a (float): takes an arguument that is a float
+        b (float): also takes a float argument
 
     Returns:
-        float: _description_
+        float: the return statement should be a float
     """
     return a + b
