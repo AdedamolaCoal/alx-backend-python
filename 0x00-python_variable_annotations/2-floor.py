90f7689b928ae8be55ab31413bddcd399b4ec520
AdedamolaCoal <117821207+AdedamolaCoal@users.noreply.github.com>
2024-10-11 08:07:21 +0100

    task 2 done

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""This function returns a floored integer
+"""
+
+
+def floor(n: float) -> int:
+    """_summary_
+
+    Args:
+      n (float): takes an argument that is a float
+
+    Returns:
+      int: floors the float to the nearest int
+    """
+    return int(n)
