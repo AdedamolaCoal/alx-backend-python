a1c0e855ea738c884b6e14d2dcafc44323b37856
AdedamolaCoal <117821207+AdedamolaCoal@users.noreply.github.com>
2024-10-15 12:39:48 +0100

    updates

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -1,13 +1,15 @@
 #!/usr/bin/env python3
-"""Returns an async generator."""
-
+"""Generates a set of random numbers
+between 0 and 10 using an async generator.
+"""
 import asyncio
 import random
 
 
 async def async_generator():
     """
-    A coroutine that asynchronously loop 10 times and returns a random async generator between 0 and 10
+    A coroutine that asynchronously loop 10 times and
+    returns a random async generator between 0 and 10
     """
     for _ in range(10):
         await asyncio.sleep(1)
--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -1,16 +1,15 @@
 #!/usr/bin/env python3
-"""Returns an async generator of 10 random numbers using async comprehension."""
+"""Generates a list of 10 random
+numbers using async comprehension module.
+"""
 
 async_generator = __import__("0-async_generator").async_generator
 
 
 async def async_comprehension():
     """
-    Collects 10 random numbers using async comprehension over async_generator.
-
-    Args: takes no argument
-
-    Returns: A list of 10 random numbers
+    Collects 10 random numbers using
+    async comprehension over async_generator.
     """
     value = [i async for i in async_generator()]
     return value
