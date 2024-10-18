131fa1d0ee8e0bdf07c84c65f2cd6c0b4ba4a5a3
AdedamolaCoal <117821207+AdedamolaCoal@users.noreply.github.com>
2024-10-18 01:56:50 +0100

    included docstrings in the code

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
-""""""
+"""Returns task_wait_random results, not wait_n."""
+
 import asyncio
 from typing import List
 
@@ -11,10 +12,11 @@ async def task_wait_n(n: int, max_delay: int) -> List[float]:
     Spawns wait_random n times with the specified max_delay.
 
     Args:
-      n (int): The number of times to spawn wait_random.
-      max_delay (int): The maximum delay for each wait_random call.
+        n (int): The number of times to spawn wait_random.
+        max_delay (int): The maximum delay for each wait_random call.
 
     Returns:
+        List[float]: The sorted list of wait_random results.
     """
     wait_times = await asyncio.gather(
         *tuple(map(lambda _: task_wait_random(max_delay), range(n)))
