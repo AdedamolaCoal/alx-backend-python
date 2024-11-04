601bdfec2f040e95a0e25b6963c2a7381d714dce
AdedamolaCoal <117821207+AdedamolaCoal@users.noreply.github.com>
2024-11-04 22:55:35 +0100

    task 6 done

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -42,6 +42,29 @@ class TestGithubOrgClient(unittest.TestCase):
             client._public_repos_url, "https://api.github.com/orgs/test-org/repos"
         )
 
+    @patch("client.get_json")
+    def test_public_repos(self, mock_get_json):
+        """Test that public_repos returns the correct list of repos."""
+        mock_repos_payload = [{"name": "repo1"}, {"name": "repo2"}, {"name": "repo3"}]
+        mock_get_json.return_value = mock_repos_payload
+
+        with patch(
+            "client.GithubOrgClient._public_repos_url", new_callable=PropertyMock
+        ) as mock_public_repos_url:
+            mock_public_repos_url.return_value = (
+                "https://api.github.com/orgs/test-org/repos"
+            )
+
+            client = GithubOrgClient("test-org")
+
+            expected_repos = ["repo1", "repo2", "repo3"]
+            self.assertEqual(client.public_repos(), expected_repos)
+
+            mock_public_repos_url.assert_called_once()
+            mock_get_json.assert_called_once_with(
+                "https://api.github.com/orgs/test-org/repos"
+            )
+
 
 if __name__ == "__main__":
     unittest.main()
